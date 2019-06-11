521ad552ac549e48e45564c2fd868486080ba84a
matheusfrancisco <matheusmachadoufsc@gmail.com>
2019-06-11 13:09:44 -0300

    feat: finish addDir and addFile

--- filesystem.hpp
+++ filesystem.hpp
@@ -27,10 +27,14 @@ class FileSystem
         unsigned char sizeof_blocks;
         string root_name;
         string file_system_name_create;
-
+        string name_file_to_add;
+        string info_to_insert_file;
         string name_dir_create;
+        //unsigned char block_free_in_root;
+        //int valor_block_add_direct_dir;
         //int num_inodes;
-        
+        int next_end_block_to_root;
+
         /*bit map informations*/
         int bitMapSize;
         unsigned char bitmap_size;
@@ -47,7 +51,6 @@ class FileSystem
         int vector_block_size;
         int vector_block_start_in_map;
         int vector_block_end_in_map;
-
         FILE *file_system_name;
         //inode _inode ;
 
@@ -61,8 +64,8 @@ class FileSystem
         void create_vector_blocks(FILE * partition);
         void insert_index_root(FILE *partition);
 
-        void change_root_direct_block(FILE* partition , unsigned char address);
-        
+        void change_root_direct_block(FILE* partition);
+        void check_root_direct_block(FILE* partition);
         /*methods*/
         void change_bit_map(FILE* partition, int number_block_free);
         void create_root(string root_name_, FILE* partition);
@@ -70,16 +73,18 @@ class FileSystem
         int find_inode_free(FILE *partition);
         void add_file_root(string file_name,string conteudo, FILE* partition);
         void insert_information_in_block(FILE* partition, int position, string info, int number_blocks_used);
-        void change_bit_map_file(FILE* partition, int number_block_free, int number_block_used);
+        void change_bit_map_adddir(FILE* partition, int number_block_free, int number_block_used);
         void change_size_root(FILE * partition);
-        
+        void change_bit_map_addfile(FILE * partition, int number_block_free, int number_need_to_insert);
 
         void command_inputs();
-        void addFile();
+        void addFile(FILE* partition, string file_name, string info_file);
         void addDir(FILE * partition, string name_dir);
-        
-        //auxiliar functions
+        void add_number_inode_dir_in_vet_block(FILE* partition, int inode_index);
+        unsigned char get_address_free_block_in_root(FILE * partition);
 
+        //auxiliar functions
+        void change_inode_direct(FILE* partition,int number_inode,  int frist_address, int numbers_used);
         void get_file_system_informations(FILE* partition);
 
         void convert_string_to_char();
